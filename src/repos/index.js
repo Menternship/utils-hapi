c94e35708b1a2d69d7106dc73a52a5b1a4f69c71
Leigh Silverstein <lsilversteinto@gmail.com>
2017-09-24 14:24:04 -0400

    remove errant or

--- src/repos/index.js
+++ src/repos/index.js
@@ -56,7 +56,7 @@ const initOptions = {
 };
 
 export const squel = blandSquel.useFlavour('postgres');
-export const predb = pgp(initOptions)(process.env.DATABASE_URL || );
+export const predb = pgp(initOptions)(process.env.DATABASE_URL);
 
 export const db = {
   none(query: string, values?: any[]) {
