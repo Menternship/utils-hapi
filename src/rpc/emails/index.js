0eaa545d3f24e38751b1293b1780888a7e931e9a
Leigh Silverstein <lsilversteinto@gmail.com>
2017-10-24 15:16:22 -0400

    add email for accepting offer intern

--- src/rpc/emails/index.js
+++ src/rpc/emails/index.js
@@ -4,6 +4,7 @@ import mirror from 'keymirror-flow';
 export const types = mirror({
   createOffer: 1,
   acceptOffer: 1,
+  acceptOfferIntern: 1,
   rejectOffer: 1,
   createApplication: 1,
   internAwaitingApproval: 1,
